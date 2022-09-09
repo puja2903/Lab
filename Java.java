4ec2f015c7569c19655a29bb3f301be746a525b9
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-09-09 15:00:35 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,12 @@
+/* Develop a public java class and make sure nobody can create 
+any object of this class from outside the class*/
+package LAB;
+
+public class Java {
+	private Java() {
+	}
+	public static void main(String[] args) {
+}}
+	
+
+
