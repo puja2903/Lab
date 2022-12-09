09a69cf8df345bc54d2c35bb29d4009bcfa016bb
puja2903 <110466374+puja2903@users.noreply.github.com>
2022-12-09 12:52:26 +0530

    Add files via upload

---
+++ 8-12-2022 lab/findEvenOdd.java
@@ -0,0 +1,16 @@
+//Problem Statement: 3
+//In Eclipse, run MyEvenOddTest class as JUnit Test.
+
+
+//1st part
+
+package com.demo.test;
+
+public class findEvenOdd {
+public static boolean EvenOdd(int a) {
+	if(a%2==0)
+		return true;
+	else
+		return false;
+}
+}
